f89ad9bcbdd1586814b816388f5a13328797a278
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2022-11-26 22:24:37 +0530

    Update data.js

--- data.js
+++ data.js
@@ -15,7 +15,7 @@ const dogsData = [
         age: 27,
         bio: "How you doin?",
     }, {
-        name: "Dio",
+        name: "Za Warudo",
         avatar: "images/dog-dio.jpg",
         age: 19,
         bio: "I like to stop time.",
